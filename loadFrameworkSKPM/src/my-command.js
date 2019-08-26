0cdb138a40dad8e93eb98e6506a649279c8d4344
Kevin Gutowski <kgutowski@berkeley.edu>
2019-08-25 20:54:30 -0700

    add plugin js

--- loadFrameworkSKPM/src/my-command.js
+++ loadFrameworkSKPM/src/my-command.js
@@ -1,6 +1,21 @@
 import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
+const NibUI = require('../assets/modalContentView.xib')
+var nib = NibUI()
+
 export default function() {
   sketch.UI.message("It's alive 🙌")
+
+  let buttonTargetFunction = (sender) => {
+    console.log("button clicked")
+    let url = NSURL.alloc().initWithString('https://www.apple.com')
+    NSWorkspace.sharedWorkspace().openURL(url)
+  }
+
+  nib.button.setCOSJSTargetFunction(sender => buttonTargetFunction(sender))
+
+  let dialog = NSAlert.alloc().init()
+  dialog.setAccessoryView(nib.getRoot())
+  dialog.runModal()
 }
